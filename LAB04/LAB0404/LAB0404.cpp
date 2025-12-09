1575ae7a6ed3d261f75d4115725bbc47fb7cd407
6812100004-tawatchaimut <6812100004@mut.ac.th>
2025-12-09 16:36:48 +0700

    Lab0404

---
+++ LAB04/LAB0404/LAB0404.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "-------- start àÅ¢¤Ùë¶ÍÂËÅÑ§ -------- \n";
+
+	for (int i = 10;
+		i >= 0;
+		i--)
+
+		if (i % 2 == 0)
+
+			cout << i << "\n";
+
+
+
+
+	return 0;
+}
