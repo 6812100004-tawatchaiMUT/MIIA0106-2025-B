cc651ba2f3262ac912809237d843b1872b40fabe
6812100004-tawatchaimut <6812100004@mut.ac.th>
2025-12-23 18:33:27 +0700

    LAB04(การบ้าน)update

---
+++ LAB04(การบ้าน)/LAB0403/LAB0403.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	int sum = 0;
+	cout << "---------- start  1 - 100 ---------- \n";
+	cout << " sum =  1 - 100 \n";
+
+	for (int i = 1;
+		i <= 100;
+		i++)
+
+	{
+		sum = sum + i;
+	}
+	cout << "ผลรวม 1-100:" << sum << " \n";
+	return 0;
+}
