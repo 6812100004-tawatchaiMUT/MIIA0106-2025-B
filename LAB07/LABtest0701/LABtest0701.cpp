e94120e52d03ba65335a2c3d1957ce8b32b187e1
6812100004-tawatchaimut <6812100004@mut.ac.th>
2025-12-28 17:03:27 +0700

    LAB7

---
+++ LAB07/LABtest0701/LABtest0701.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	int x = 10;
+
+	cout << "address of x is " << &x << endl;
+	cout << "value of x is " << x << endl;
+
+	// pointer
+	int* pt = &x;
+
+	cout << " address of pt is" << pt << endl;
+	cout << "value of pt is " << pt << endl;
+	cout << "value of x is " << x << endl;
+	cout << "value of y is " << y << endl;
+
+	pt = &y;
+	*pt = 500;
+
+	cout << "value of x is " << x << endl;
+	cout << "value of y is " << y << endl;
+
+	getchar();
+
+	return 0;
+}
