0f48f1edb0f8bdf06c0f69df6f4920dd5ebf3363
6812100004-tawatchaimut <6812100004@mut.ac.th>
2026-01-04 21:30:53 +0700

    LAB0807(1คะแนน)

---
+++ LAB08(ข้อ7)/LAB0807/LAB0807.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+struct Phone
+{
+	string home;
+	string mobile;
+};
+
+struct Parrent
+{
+	string name;
+	string relationship;
+	Phone contact;
+};
+struct student
+{
+	string id;
+	string nickname;
+	string lineid;
+	Phone myContact;
+	Parrent myparent;
+};
+
+int main()
+{
+	student s1;
+	// student data
+	s1.id = "67010001";
+	s1.nickname = "Karn";
+	s1.lineid = "gant.dev";
+
+	// student contact info
+	s1.myContact.home = "02-123-4567";
+	s1.myContact.mobile = "081-234-5678";
+
+	// parent data
+	s1.myparent.name = "Mr.Somchai";
+	s1.myparent.relationship = "Father";
+
+	// parent contact info
+	s1.myparent.contact.home = "02-123-4567";
+	s1.myparent.contact.mobile = "089-987-6543";
+
+	// print student info
+	cout << "Student ID: " << s1.id << endl;
+	cout << "Nickname: " << s1.nickname << endl;
+	cout << "Student Mobile: " << s1.myContact.mobile << endl;
+	cout << "Parent Name: " << s1.myparent.name << endl;
+	cout << "Parent Mobile: " << s1.myparent.contact.mobile << endl;
+
+	return 0;
+}
